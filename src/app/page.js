cbcb71a8411adafcbd6ab531ef1c7968fccfe281
rego467 <regoedwar.1@gmail.com>
2024-03-06 19:44:31 +0700

    perubahan di loadmore

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import styles from "@/app/styles/component.module.css"
 import { inter } from "./font/font";
 import Animes from "@/components/Animes";
 import Link from "next/link";
+import LoadMore from "@/components/LoadMore";
 
 export async function useAnime(){
   try {
@@ -30,10 +31,11 @@ export default async function Page() {
         <div className={`${inter.variable} flex mt-5 p-1 items-center justify-between`}>
           <div className="flex justify-between w-full items-center">
             <h1 className={`${styles.headingPopuler} underline`}>POPULER</h1>
-            <Link href={"/anime"}><h1 className={`${styles.headingPopuler} underline`}>ALL ANIME</h1></Link>
+            {/* <Link href={"/anime"}><h1 className={`${styles.headingPopuler} underline`}>ALL ANIME</h1></Link> */}
           </div>
         </div>
           <Animes animes={data}/>
+          <LoadMore />
       </div>   
     </div>
   )
